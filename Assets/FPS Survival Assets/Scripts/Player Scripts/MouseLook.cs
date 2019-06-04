d7bea5f5ff73091e95a715b13c627c3b29c6bd68
Lloyd Risper <cashmakersent@hotmail.com>
2019-06-04 16:00:28 -0400

    Mouse Controls
    Mouse Look, Mouse Controls

---
+++ Assets/FPS Survival Assets/Scripts/Player Scripts/MouseLook.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private Transform _playerRoot;
+    [SerializeField] private Transform _lookRoot;
+
+    [SerializeField] private bool _invert;
+    [SerializeField] private bool _canUnlock = true;
+
+    [SerializeField] private float _sensivity = 5f;
+    [SerializeField] private float _smootheWeight = 0.4f;
+    [SerializeField] private float _rollAngle = 10f;
+    [SerializeField] private float _rollSpeed = 3f;
+
+    [SerializeField] private int _smoothSteps = 10;
+
+    [SerializeField] private Vector2 _defaultLookLimits = new Vector2(-70f, 80);
+
+    private Vector2 _lookAngles;
+    private Vector2 _currentMouseLook;
+    private Vector2 _smoothMove;
+
+    private float _currentRollAngle;
+    private int _lastFrame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LockAndUnlockCursor();
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            LookAround();
+        }
+    }
+
+    void LockAndUnlockCursor()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+
+    void LookAround()
+    {
+        _currentMouseLook = new Vector2(Input.GetAxis(MouseAxis.MOUSE_Y), Input.GetAxis(MouseAxis.MOUSE_X));
+
+        _lookAngles.x += _currentMouseLook.x * _sensivity * (_invert ? 1f : -1f);
+        _lookAngles.y += _currentMouseLook.y * _sensivity;
+
+        _lookAngles.x = Mathf.Clamp(_lookAngles.x, _defaultLookLimits.x, _defaultLookLimits.y);
+
+        //_currentRollAngle = Mathf.Lerp(_currentRollAngle, Input.GetAxisRaw(MouseAxis.MOUSE_X) * _rollAngle, Time.deltaTime * _rollSpeed);
+
+        _lookRoot.rotation = Quaternion.Euler(_lookAngles.x, 0f, _currentRollAngle);
+        _playerRoot.rotation = Quaternion.Euler(0f, _lookAngles.y, 0f);
+    }
+}
---
+++ Assets/FPS Survival Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController _characterController;
+    private Vector3 _moveDirection;
+
+    public float Speed = 5f;
+    private float _gravity = 20f;
+    public float jumpForce = 10f;
+    private float _verticalVelocity;
+
+    void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveThePlayer();
+    }
+
+    void MoveThePlayer()
+    {
+        _moveDirection = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL));
+        _moveDirection = transform.TransformDirection(_moveDirection);
+    }
+}
