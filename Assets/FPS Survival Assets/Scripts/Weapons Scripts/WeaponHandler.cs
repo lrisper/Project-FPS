6a3c22c0afd4a71bb2cf787c682a334f1ddaa3d9
Lloyd Risper <cashmakersent@hotmail.com>
2019-06-05 16:12:09 -0400

    Weapon Handler
    connected the weapons

---
+++ Assets/FPS Survival Assets/Scripts/Weapons Scripts/WeaponHandler.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponAim
+{
+    NONE,
+    SELF_AIM,
+    AIM
+}
+
+public enum WeaponFireType
+{
+    SINGLE,
+    MULTIPLE
+}
+
+public enum WeaponBulletType
+{
+    BULLET,
+    ARROW,
+    SPEAR,
+    NONE
+}
+
+public class WeaponHandler : MonoBehaviour
+{
+
+    private Animator _anim
+    ;
+    public WeaponAim weaponAim;
+    [SerializeField] private GameObject _muzzleFlash;
+
+    public AudioSource shotSound, reloadSound;
+
+    public WeaponFireType weaponFireType;
+    public WeaponBulletType weaponBulletType;
+    public GameObject attackPoint;
+
+    void Awake()
+    {
+        _anim = GetComponent<Animator>();
+
+    }
+
+    public void ShootAnimation()
+    {
+        _anim.SetTrigger(AnimationTags.SHOOT_Trigger);
+    }
+    public void Aim(bool canAim)
+    {
+        _anim.SetBool(AnimationTags.AIM_PARAMETER, canAim);
+
+    }
+
+    public void TurnOnMuzzleFlash()
+    {
+        _muzzleFlash.SetActive(true);
+    }
+
+    public void TurnOffMuzzleFlash()
+    {
+        _muzzleFlash.SetActive(false);
+    }
+
+    void PlayShootSound()
+    {
+        shotSound.Play();
+    }
+
+    void PlayReloadSound()
+    {
+        reloadSound.Play();
+    }
+
+    void TurnOnAttackPoint()
+    {
+        attackPoint.SetActive(true);
+    }
+
+    void TurnOffAttackPoint()
+    {
+        if (attackPoint.activeInHierarchy)
+        {
+            attackPoint.SetActive(false);
+        }
+
+    }
+}
