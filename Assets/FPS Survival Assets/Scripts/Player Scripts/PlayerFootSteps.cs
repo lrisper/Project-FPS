f6c94f7025e150b2a74895834c809fb466287fc7
Lloyd Risper <cashmakersent@hotmail.com>
2019-06-05 02:53:39 -0400

    Footstep sounds
    Able to hear player footstep sounds

---
+++ Assets/FPS Survival Assets/Scripts/Player Scripts/PlayerFootSteps.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerFootSteps : MonoBehaviour
+{
+    private AudioSource _footStepSounds;
+
+    [SerializeField] private AudioClip[] _footStepClip;
+
+    private CharacterController _characterController;
+
+    [HideInInspector] public float VolMin, VolMax;
+    private float _accumultedDistance;
+    [HideInInspector] public float StepDistance;
+
+
+    void Awake()
+    {
+        _footStepSounds = GetComponent<AudioSource>();
+        _characterController = GetComponentInParent<CharacterController>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CkeckToPlayFootStepSounds();
+
+
+    }
+
+    private void CkeckToPlayFootStepSounds()
+    {
+        if (!_characterController.isGrounded)
+        {
+            return;
+        }
+
+        if (_characterController.velocity.sqrMagnitude > 0)
+        {
+            _accumultedDistance += Time.deltaTime;
+            if (_accumultedDistance > StepDistance)
+            {
+                _footStepSounds.volume = UnityEngine.Random.Range(VolMin, VolMax);
+                _footStepSounds.clip = _footStepClip[UnityEngine.Random.Range(0, _footStepClip.Length)];
+                _footStepSounds.Play();
+
+                _accumultedDistance = 0;
+            }
+        }
+        else
+        {
+            _accumultedDistance = 0;
+        }
+    }
+}
--- Assets/FPS Survival Assets/Scripts/Player Scripts/PlayerSprintAndCroch.cs
+++ Assets/FPS Survival Assets/Scripts/Player Scripts/PlayerSprintAndCroch.cs
@@ -16,11 +16,34 @@ public class PlayerSprintAndCroch : MonoBehaviour
 
     private bool _isCrouching;
 
+    private PlayerFootSteps _playerFootSteps;
+
+    private float _sprintVolume = 1f;
+    private float _crouchVolume = 0.1f;
+
+    private float _walkVolumeMin = 0.2f;
+    private float _walkVolumeMax = 0.6f;
+
+    private float _walkStepDistance = 0.4f;
+    private float _sprintStepDistance = 0.25f;
+    private float _crouchStepDistance = 0.5f;
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         _playerMovment = GetComponent<PlayerMovement>();
         _lookRoot = transform.GetChild(0);
+
+        _playerFootSteps = GetComponentInChildren<PlayerFootSteps>();
+    }
+
+    public void Start()
+    {
+        _playerFootSteps.VolMin = _walkVolumeMin;
+        _playerFootSteps.VolMax = _walkVolumeMax;
+
+        _playerFootSteps.StepDistance = _walkStepDistance;
     }
 
     // Update is called once per frame
@@ -35,10 +58,21 @@ public class PlayerSprintAndCroch : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftShift) && !_isCrouching)
         {
             _playerMovment.Speed = SprintSpeed;
+
+            _playerFootSteps.StepDistance = _sprintStepDistance;
+            _playerFootSteps.VolMin = _sprintVolume;
+            _playerFootSteps.VolMax = _sprintVolume;
         }
         if (Input.GetKeyUp(KeyCode.LeftShift) && !_isCrouching)
         {
             _playerMovment.Speed = MoveSpeed;
+
+            _playerFootSteps.StepDistance = _walkStepDistance;
+
+            _playerFootSteps.VolMin = _walkVolumeMin;
+            _playerFootSteps.VolMax = _walkVolumeMax;
+
+
         }
     }
 
@@ -50,12 +84,22 @@ public class PlayerSprintAndCroch : MonoBehaviour
             {
                 _lookRoot.localPosition = new Vector3(0f, _standHeight, 0f);
                 _playerMovment.Speed = MoveSpeed;
+
+                _playerFootSteps.StepDistance = _walkStepDistance;
+                _playerFootSteps.VolMin = _sprintVolume;
+                _playerFootSteps.VolMax = _sprintVolume;
+
                 _isCrouching = false;
             }
             else
             {
                 _lookRoot.localPosition = new Vector3(0f, _crouchHeight, 0f);
                 _playerMovment.Speed = CrochSpeed;
+
+                _playerFootSteps.StepDistance = _crouchStepDistance;
+                _playerFootSteps.VolMin = _crouchVolume;
+                _playerFootSteps.VolMax = _crouchVolume;
+
                 _isCrouching = true;
             }
         }
