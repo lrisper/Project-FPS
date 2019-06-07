4dbeccc07688f1e872c3f9f7e0986a7a6d6946a5
Lloyd Risper <cashmakersent@hotmail.com>
2019-06-06 23:08:08 -0400

    Player UI
    Createdplayer stats UI

---
+++ Assets/FPS Survival Assets/Scripts/Player Scripts/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    [SerializeField] private Image HealthStats, StaminaStats;
+
+    public void DisplayHealthStats(float healthVaule)
+    {
+        healthVaule /= 100f;
+        HealthStats.fillAmount = healthVaule;
+    }
+
+    public void DisplayStaminaStats(float staminaVaule)
+    {
+        staminaVaule /= 100f;
+        StaminaStats.fillAmount = staminaVaule;
+    }
+}
